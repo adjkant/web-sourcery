7b5a1a479b0795c5aec41cb2f71d883c85f9fc22
Adrian Kant <adjkant@gmail.com>
2019-01-28 10:49:36 -0500

    basic prototype

--- examples/0-0-1-example.rkt
+++ examples/0-0-1-example.rkt
@@ -1,3 +1,18 @@
 #lang racket
 
 (require "../lib/web-sourcery.rkt")
+(sourcery-db "0-0-1-server.db")
+
+(sourcery-struct session [(path STRING)])
+
+(create-web-sourcery-app app)
+
+(route app "/example"
+(define (test-1)
+  (session-path (session-create "/example"))))
+
+(route app "/data"
+(define (test-2)
+  (foldl (λ (s cur) (string-append (session-path s) " " cur)) "" (sourcery-load session))))
+
+(run-web-sourcery-app app)
--- lib/web-sourcery.rkt
+++ lib/web-sourcery.rkt
@@ -1,11 +1,50 @@
 #lang racket
 
-(provide )
+(provide route
+         create-web-sourcery-app
+         run-web-sourcery-app
+         (all-from-out "../include/sql-sourcery/lib/sql-sourcery.rkt"))
 
 (require web-server/servlet
-         web-server/servlet-env)
+         web-server/servlet-env
+         (for-syntax syntax/parse)
+         "../include/sql-sourcery/lib/sql-sourcery.rkt")
+
+(define-syntax create-web-sourcery-app
+  (syntax-parser
+    [(_ app-name) #'(define app-name '())]))
+
+(define-syntax route
+  (syntax-parser
+    [(_ app-name:id path:string (define (route-func) route-body))
+        #'(begin
+            (define (route-func) route-body)
+            (set! app-name (cons (list path route-func) app-name)))]))
+
+(define (handle-route req app)
+  (let* [(path (first (map path/param-path (url-path (request-uri req)))))
+         (matched-route (filter (λ (r) (string=? (substring (first r) 1) path)) app))]
+    (if (> (length matched-route) 0)
+        (string->bytes/utf-8 ((second (first matched-route))))
+        (string->bytes/utf-8 "no matching route"))))
+
+(define-syntax run-web-sourcery-app
+  (syntax-parser
+    [(_ app-name) #'(begin
+                      (define (app-main req)
+                        (response/full 200
+                                       #"Ok"
+                                       (current-seconds)
+                                       TEXT/HTML-MIME-TYPE
+                                       '()
+                                       (list (handle-route req app-name))))
+                      (serve/servlet app-main #:port 80
+                                     #:command-line? #t
+                                     #:servlet-regexp #rx""
+                                     #:listen-ip #f
+                                     #:stateless? #t))]))
  
-(define (my-app req)
+#|(define (my-app req)
   (response/full 200
                  #"Ok"
                  (current-seconds)
@@ -20,4 +59,4 @@
                #:command-line? #t
                #:servlet-regexp #rx""
                #:listen-ip #f
-               #:stateless? #t)
+               #:stateless? #t)|#
